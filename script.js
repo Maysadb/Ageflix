b20a49769602a0d177ee254bbdd973812be0f6ac
Maysa <maysa.debsawi@gmail.com>
2025-10-30 11:02:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function autoSlideSliders() {
 }
 
 window.addEventListener('load', autoSlideSliders);
+
+async function init() {
+  try {
+    model = await tmImage.load(URL + "model.json", URL + "metadata.json");
+    console.log("Model loaded successfully");
+  } catch (error) {
+    console.error("Failed to load the model:", error);
+    alert("Error loading model. Check console.");
+  }
+
+  webcam = new tmImage.Webcam(320, 240, true);
+  await webcam.setup();
+  webcam.play();
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  window.requestAnimationFrame(loop);
+}
